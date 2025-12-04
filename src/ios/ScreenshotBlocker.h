78b5f9a3bf4d6a9da6c7edfd842d0837f7aca5b3
cyruscvc <cyruscvc@gmail.com>
2025-12-04 11:55:18 +0400

    Update ScreenshotBlocker.h

--- src/ios/ScreenshotBlocker.h
+++ src/ios/ScreenshotBlocker.h
@@ -1,10 +1,10 @@
-#import <Cordova/CDV.h>
-#import "ScreenRecordingDetector.h"
-
+#import <Cordova/CDVPlugin.h>
+#import <UIKit/UIKit.h>
 
 @interface ScreenshotBlocker : CDVPlugin
 
-- (void)enable:(CDVInvokedUrlCommand*)command;
--(void)listen:(CDVInvokedUrlCommand*)command;
+- (void)enable:(CDVInvokedUrlCommand*)command;  // Existing? Extend for prevention
+- (void)disable:(CDVInvokedUrlCommand*)command; // Existing? Extend for disable
+- (void)isEnabled:(CDVInvokedUrlCommand*)command; // Optional check
 
 @end
